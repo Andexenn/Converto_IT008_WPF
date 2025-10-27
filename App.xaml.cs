8173f9eee0b7274a2b1bd83992298d58b5d3e0cc
Andexenn <phanhuynhhung0707@gmail.com>
2025-10-28 00:15:56 +0700

    Add project files.

---
+++ App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Converto_IT008_WPF
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}

3fc69ffec131aa36d534c53706c5f8f6137858aa
Andexenn <phanhuynhhung0707@gmail.com>
2025-10-28 08:44:12 +0700

    1. Feat(MainWindow): Add MainWindow view

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,14 +1,27 @@
 ﻿using System.Configuration;
 using System.Data;
 using System.Windows;
+using Converto_IT008_WPF.DI;
+using Microsoft.Extensions.DependencyInjection;
 
-namespace Converto_IT008_WPF
+namespace Converto_IT008_WPF;
+
+/// <summary>
+/// Interaction logic for App.xaml
+/// </summary>
+public partial class App : Application
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-    }
+    public IServiceProvider? ServiceProvider { get; private set; }
 
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        var services = new ServiceCollection();
+
+        services.RegisterDependency();
+        ServiceProvider = services.BuildServiceProvider();
+        var mainWindow = ServiceProvider.GetRequiredService<Views.MainWindow>();
+        mainWindow.Show();
+
+    }
 }
---
+++ DI/AppModule.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Converto_IT008_WPF.DI;
+
+public static class AppModule
+{
+    public static IServiceCollection RegisterDependency(this IServiceCollection services)
+    {
+        services.AddTransient<Views.MainWindow>();
+
+        return services;
+    }
+}

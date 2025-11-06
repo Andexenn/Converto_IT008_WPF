58a7356d4ba9306eaeeb81e86b671f2511c306bd
Andexenn <phanhuynhhung0707@gmail.com>
2025-11-06 07:32:02 +0700

    25. Feat(Connection): Create connection for database

---
+++ Backend/App/Database/connection.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+from App.config import settings 
+
+SQL_ALCHEMY_DATABASE_URL = f"mysql+pymysql://{settings.DB_USER}:{settings.DB_PASSWORD}@{settings.DB_HOST}:{settings.DB_PORT}/{settings.DB_NAME}"
+
+engine = create_engine(
+    SQL_ALCHEMY_DATABASE_URL,
+    pool_pre_ping=True,
+    echo=settings.DEBUG)
+
+SessionLocal = sessionmaker(autoflush=False, bind=engine)
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db 
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
+
+
+
+
+

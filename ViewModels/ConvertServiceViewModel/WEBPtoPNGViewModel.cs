41b3cb37c23a1df96a3b1369b08985ed0a13ea03
Andexenn <phanhuynhhung0707@gmail.com>
2025-11-09 12:44:14 +0700

    47. Feat(UI): Some animation for button and open folder explorer

--- ViewModels/ConvertServiceViewModel/WEBPtoPNGViewModel.cs
+++ ViewModels/ConvertServiceViewModel/WEBPtoPNGViewModel.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Win32;
 
 namespace Converto_IT008_WPF.ViewModels.ConvertServiceViewModel;
 
@@ -20,5 +21,24 @@ public partial class WEBPtoPNGViewModel : BaseViewModel
     void GoBack()
     {
         _navigationService.Navigate<HomepageViewModel>();
+    }
+
+    [RelayCommand]
+    void OpenFolder(string folderPath)
+    {
+        try
+        {
+            var dlg = new OpenFileDialog();
+            dlg.Filter = "All files (*.*)|*.*";
+            if (dlg.ShowDialog() == true)
+            {
+                var file = dlg.FileName;
+            }
+        }
+        catch (Exception ex)
+        {
+            // Xử lý lỗi khi mở thư mục
+            System.Windows.MessageBox.Show($"Không thể mở thư mục: {ex.Message}", "Lỗi", System.Windows.MessageBoxButton.OK, System.Windows.MessageBoxImage.Error);
+        }
     }
 }

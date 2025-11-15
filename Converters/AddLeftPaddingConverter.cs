85807a6a557d0a98cd75770094cba9273815897b
Thuan Nguyen <NightMareScorn@gmail.com>
2025-11-15 12:03:44 +0700

    63. Add Placeholder TextBox Control

---
+++ Converters/AddLeftPaddingConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using System.Windows;
+using System.Windows.Data;
+
+namespace PlaceholderTextBoxControl.Converters
+{
+    internal class AddLeftPaddingConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (!(value is Thickness padding))
+            {
+                return value;
+            }
+
+            if (!double.TryParse(parameter.ToString(), out double amount))
+            {
+                return value;
+            }
+
+            padding.Left += amount;
+
+            return padding;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

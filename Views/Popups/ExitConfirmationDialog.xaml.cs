d6ee2e4887f6901e338c470132e40f7089c1f0f3
Thuan Nguyen <NightMareScorn@gmail.com>
2025-11-26 13:06:46 +0700

    73.1. Add ExitConfirm

---
+++ Views/Popups/ExitConfirmationDialog.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Converto
+{
+    public partial class ExitConfirmationDialog : Window
+    {
+        public ExitConfirmationDialog()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnYes_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void BtnNo_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+    }
+}

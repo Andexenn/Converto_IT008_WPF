c58fcf28c5650c07f97349b8d36c93e4d5b2a25d
Thuan Nguyen <NightMareScorn@gmail.com>
2025-11-29 21:15:57 +0700

    77. Add Markdown to Text Feature

---
+++ ViewModels/SideServices/MarkdownToTextViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Converto_IT008_WPF.ViewModels;
+
+namespace Converto_IT008_WPF.ViewModels.SideServices
+{
+    public class MarkdownToTextViewModel : BaseViewModel
+    {
+        public MarkdownToTextViewModel()
+        {
+
+        }
+    }
+}

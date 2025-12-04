87ea91eba3637b092cca95911e6d9af2f324ba89
Thuan Nguyen <NightMareScorn@gmail.com>
2025-12-04 21:20:21 +0700

    86. Update MarkdownToText/ Disconnect/ ExitConfirm Page

--- DI/AppModule.cs
+++ DI/AppModule.cs
@@ -7,6 +7,7 @@ using Converto_IT008_WPF.ServicesFEImpl.UserServiceImpls;
 using Converto_IT008_WPF.Stores;
 using Converto_IT008_WPF.ViewModels;
 using Converto_IT008_WPF.Views;
+using Converto_IT008_WPF.Views.UserViews;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Http;
 using System;
@@ -63,6 +64,9 @@ public static class AppModule
         //------------------ Views ------------------
         services.AddSingleton<Views.MainWindow>();
         services.AddSingleton<Views.Popups.Terms_ConditionsPopup>();
+        services.AddTransient<Views.UserViews.LoginView>();
+        services.AddTransient<Views.UserViews.SignUpView>();
+
 
         return services;
     }

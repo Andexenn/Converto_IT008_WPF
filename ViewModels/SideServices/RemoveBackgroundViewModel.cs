e85966bdfdd537e040389606b334f393699a76de
Thuan Nguyen <NightMareScorn@gmail.com>
2025-12-17 01:13:20 +0700

    110.2. Update History Status and Delete MainWindow Margin in Page Navigation

--- ViewModels/SideServices/RemoveBackgroundViewModel.cs
+++ ViewModels/SideServices/RemoveBackgroundViewModel.cs
@@ -2,282 +2,290 @@
 using CommunityToolkit.Mvvm.Input;
 using Converto_IT008_WPF.Dto;
 using Converto_IT008_WPF.ServicesFE;
-using System.IO;
-using System.Collections.Generic;
+using System;
 using System.Collections.ObjectModel;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
-using System.Windows.Documents;
 using System.Windows.Forms;
 using System.Windows.Media.Imaging;
 
-namespace Converto_IT008_WPF.ViewModels.SideServices;
-
-public partial class RemoveBackgroundViewModel : BaseViewModel
+namespace Converto_IT008_WPF.ViewModels.SideServices
 {
-    string[] filepaths = { };
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(NotUploaded))]
-    private bool uploaded;
-    public bool NotUploaded => !Uploaded;
-
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(NotRemoved))]
-    private bool removed;
-
-    public bool NotRemoved => !Removed;
-
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(IsNotDownloading))]
-    private bool isDownloading;
-    public bool IsNotDownloading => !IsDownloading;
-
-    [ObservableProperty]
-    [NotifyPropertyChangedFor(nameof(IsNotRemoving))]
-    private bool isRemoving;
-    public bool IsNotRemoving => !IsRemoving;
-
-    [ObservableProperty]
-    ObservableCollection<ProcessedFileResultDto> processedImages = new ObservableCollection<ProcessedFileResultDto>();
-    [ObservableProperty]
-    ObservableCollection<BitmapImage> unprocessedImages = new ObservableCollection<BitmapImage>();
-    [ObservableProperty]
-    ObservableCollection<UserTasksDto> userTasks = new ObservableCollection<UserTasksDto>();
-
-    [ObservableProperty]
-    BitmapImage beforeRemoved = new BitmapImage();
-    [ObservableProperty]
-    BitmapImage afterRemoved = new BitmapImage();
-    int beforeIndex = 0;
-    int afterIndex = 0;
-
-    IRemoveBackgroundService _removeBackgroundService;
-    IProcessImageService _processImageService;
-    ITaskService _taskService;
-
-    public RemoveBackgroundViewModel(IRemoveBackgroundService removeBackgroundService, IProcessImageService processImageService, ITaskService taskService)
+    public partial class RemoveBackgroundViewModel : BaseViewModel
     {
-        _removeBackgroundService = removeBackgroundService;
-        _processImageService = processImageService;
-        _taskService = taskService;
+        // --- Properties ---
+        string[] filepaths = { };
 
-        _ = GetUserTasks();
-    }
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(NotUploaded))]
+        private bool uploaded;
+        public bool NotUploaded => !Uploaded;
 
-    [RelayCommand]
-    void DropFile(object data)
-    {
-        try
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(NotRemoved))]
+        private bool removed;
+        public bool NotRemoved => !Removed;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotDownloading))]
+        private bool isDownloading;
+        public bool IsNotDownloading => !IsDownloading;
+
+        [ObservableProperty]
+        [NotifyPropertyChangedFor(nameof(IsNotRemoving))]
+        private bool isRemoving;
+        public bool IsNotRemoving => !IsRemoving;
+
+        // --- Collections ---
+        [ObservableProperty]
+        ObservableCollection<ProcessedFileResultDto> processedImages = new ObservableCollection<ProcessedFileResultDto>();
+
+        [ObservableProperty]
+        ObservableCollection<BitmapImage> unprocessedImages = new ObservableCollection<BitmapImage>();
+
+        [ObservableProperty]
+        ObservableCollection<RemoveBackgroundTaskItem> userTasks = new ObservableCollection<RemoveBackgroundTaskItem>();
+
+        // --- Image Previews ---
+        [ObservableProperty]
+        BitmapImage beforeRemoved = new BitmapImage();
+
+        [ObservableProperty]
+        BitmapImage afterRemoved = new BitmapImage();
+
+        int beforeIndex = 0;
+        int afterIndex = 0;
+
+        // --- Services ---
+        private readonly IRemoveBackgroundService _removeBackgroundService;
+        private readonly IProcessImageService _processImageService;
+        private readonly ITaskService _taskService;
+
+        // --- Constructor ---
+        public RemoveBackgroundViewModel(IRemoveBackgroundService removeBackgroundService, IProcessImageService processImageService, ITaskService taskService)
         {
-            string[] files = data as string[];
-            if (files != null && files.Length > 0)
-            {
-                filepaths = files;
-                Debug.WriteLine($"File dropped: {filepaths[0]}");
-                Uploaded = true;
-                BeforeRemoved = LoadBitmapImage(filepaths[0]);
-            }
-        }
-        catch(Exception ex)
-        {
-            Debug.WriteLine($"Error dropping file: {ex.Message}");
-        }
-    }
+            _removeBackgroundService = removeBackgroundService;
+            _processImageService = processImageService;
+            _taskService = taskService;
 
-    [RelayCommand]
-    void OnPickFile()
-    {
-        try
-        {
-            var OpenFileDialog = new OpenFileDialog
-            {
-                Multiselect = true, 
-                Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
-                Title = "Choose images to remove background"
-            };
-
-
-            if (OpenFileDialog.ShowDialog() == DialogResult.OK)
-            {
-                filepaths = OpenFileDialog.FileNames;
-                Debug.WriteLine($"File picked: {filepaths[0]}");
-                Uploaded = true;
-                BeforeRemoved = LoadBitmapImage(filepaths[0]);
-            }
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Error picking file: {ex.Message}");
-        }
-    }
-
-    [RelayCommand]
-    void ClearUploadFiles()
-    {
-        filepaths = Array.Empty<string>();
-        Uploaded = false;
-        Debug.WriteLine("Upload files cleared.");
-    }
-
-    [RelayCommand]
-    async Task RemoveBackground()
-    {
-        try
-        {
-            IsRemoving = true;
-            if(filepaths.Length == 0)
-            {
-                Debug.WriteLine("No files to process.");
-                return;
-            }
-
-            byte[] response = await _removeBackgroundService.RemoveBackgroundAsync(filepaths);
-            Debug.WriteLine("Background removal process initiated.");
-
-            if(response == null || response.Length == 0)
-            {
-                Debug.WriteLine("No response received from background removal service.");
-                return;
-            }
-
-            ProcessedImages.Clear();
-
-            if (isZipFile(response))
-                _processImageService.ProcessZipResponse(ProcessedImages ,response);
-            else
-                _processImageService.ProcessSingleImageResponse(ProcessedImages, response, Path.GetFileNameWithoutExtension(filepaths[0]), Path.GetExtension(filepaths[0]), "removedbg");
-
-            
-
-            AfterRemoved = ProcessedImages[0].DisplayImage;
-        }
-        catch(Exception ex)
-        {
-            Debug.WriteLine($"Error removing background: {ex.Message}");
-        }
-        finally
-        {
-            Removed = true;
-            IsRemoving = false;
             _ = GetUserTasks();
         }
-    }
 
-    [RelayCommand]
-    private void DeleteCurrentPreviewedFile()
-    {
-        if (filepaths.Length == 0)
-            return;
-        if (filepaths.Length == 1)
+        // --- Commands ---
+        [RelayCommand]
+        void DropFile(object data)
         {
-            ClearUploadFiles();
+            try
+            {
+                if (data is string[] files && files.Length > 0)
+                {
+                    filepaths = files;
+                    Uploaded = true;
+                    BeforeRemoved = LoadBitmapImage(filepaths[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error dropping file: {ex.Message}");
+            }
+        }
+
+        [RelayCommand]
+        void OnPickFile()
+        {
+            try
+            {
+                var openFileDialog = new OpenFileDialog
+                {
+                    Multiselect = true,
+                    Filter = "Image Files|*.jpg;*.jpeg;*.png;*.bmp|All Files|*.*",
+                    Title = "Choose images to remove background"
+                };
+
+                if (openFileDialog.ShowDialog() == DialogResult.OK)
+                {
+                    filepaths = openFileDialog.FileNames;
+                    Uploaded = true;
+                    BeforeRemoved = LoadBitmapImage(filepaths[0]);
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error picking file: {ex.Message}");
+            }
+        }
+
+        [RelayCommand]
+        void ClearUploadFiles()
+        {
             filepaths = Array.Empty<string>();
             Uploaded = false;
-            return;
         }
-        filepaths = filepaths.Where((source, index) => index != beforeIndex).ToArray();
 
-        NextBeforeImage();
-    }
-
-    [RelayCommand]
-    private void NextBeforeImage()
-    {
-        Debug.WriteLine($"filepaths length: {filepaths.Length}");
-        if (filepaths.Length == 0) return;
-        beforeIndex = (beforeIndex + 1) % filepaths.Length;
-        BeforeRemoved = LoadBitmapImage(filepaths[beforeIndex]);
-    }
-
-    [RelayCommand]
-    private void PreviousBeforeImage()
-    {
-        Debug.WriteLine($"filepaths length: {filepaths.Length}");
-        if (filepaths.Length == 0) return;
-        beforeIndex = (beforeIndex - 1 + filepaths.Length) % filepaths.Length;
-        BeforeRemoved = LoadBitmapImage(filepaths[beforeIndex]);
-    }
-
-    [RelayCommand]
-    private void NextAfterImage()
-    {
-        if (ProcessedImages.Count == 0) return;
-        afterIndex = (afterIndex + 1) % ProcessedImages.Count;
-        AfterRemoved = ProcessedImages[afterIndex].DisplayImage;
-    }
-
-    [RelayCommand]
-    private void PreviousAfterImage() {
-        if (ProcessedImages.Count == 0) return;
-        afterIndex = (afterIndex - 1 + ProcessedImages.Count) % ProcessedImages.Count;
-        AfterRemoved = ProcessedImages[afterIndex].DisplayImage;
-    }
-
-    [RelayCommand]
-    private async Task DownloadImages()
-    {
-        try
+        [RelayCommand]
+        async Task RemoveBackground()
         {
-            IsDownloading = true;
-            await _processImageService.DownloadImages(ProcessedImages);
+            try
+            {
+                IsRemoving = true;
+                if (filepaths.Length == 0) return;
+
+                byte[] response = await _removeBackgroundService.RemoveBackgroundAsync(filepaths);
+
+                if (response == null || response.Length == 0) return;
+
+                ProcessedImages.Clear();
+
+                if (isZipFile(response))
+                    _processImageService.ProcessZipResponse(ProcessedImages, response);
+                else
+                    _processImageService.ProcessSingleImageResponse(ProcessedImages, response, Path.GetFileNameWithoutExtension(filepaths[0]), Path.GetExtension(filepaths[0]), "removedbg");
+
+                AfterRemoved = ProcessedImages[0].DisplayImage;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error removing background: {ex.Message}");
+            }
+            finally
+            {
+                Removed = true;
+                IsRemoving = false;
+                await GetUserTasks();
+            }
         }
-        catch (Exception ex)
+
+        [RelayCommand]
+        private void DeleteCurrentPreviewedFile()
         {
-            Debug.WriteLine($"Error downloading images: {ex.Message}");
+            if (filepaths.Length == 0) return;
+            if (filepaths.Length == 1)
+            {
+                ClearUploadFiles();
+                return;
+            }
+            filepaths = filepaths.Where((source, index) => index != beforeIndex).ToArray();
+            NextBeforeImage();
         }
-        finally
+
+        [RelayCommand]
+        private void NextBeforeImage()
         {
-            IsDownloading = false;
+            if (filepaths.Length == 0) return;
+            beforeIndex = (beforeIndex + 1) % filepaths.Length;
+            BeforeRemoved = LoadBitmapImage(filepaths[beforeIndex]);
+        }
+
+        [RelayCommand]
+        private void PreviousBeforeImage()
+        {
+            if (filepaths.Length == 0) return;
+            beforeIndex = (beforeIndex - 1 + filepaths.Length) % filepaths.Length;
+            BeforeRemoved = LoadBitmapImage(filepaths[beforeIndex]);
+        }
+
+        [RelayCommand]
+        private void NextAfterImage()
+        {
+            if (ProcessedImages.Count == 0) return;
+            afterIndex = (afterIndex + 1) % ProcessedImages.Count;
+            AfterRemoved = ProcessedImages[afterIndex].DisplayImage;
+        }
+
+        [RelayCommand]
+        private void PreviousAfterImage()
+        {
+            if (ProcessedImages.Count == 0) return;
+            afterIndex = (afterIndex - 1 + ProcessedImages.Count) % ProcessedImages.Count;
+            AfterRemoved = ProcessedImages[afterIndex].DisplayImage;
+        }
+
+        [RelayCommand]
+        private async Task DownloadImages()
+        {
+            try
+            {
+                IsDownloading = true;
+                await _processImageService.DownloadImages(ProcessedImages);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error downloading images: {ex.Message}");
+            }
+            finally
+            {
+                IsDownloading = false;
+            }
+        }
+
+        // --- Methods ---
+        private async Task GetUserTasks()
+        {
+            try
+            {
+                IsBusy = true;
+                var tasks = await _taskService.GetUserTasksAsync();
+
+                var filteredTasks = tasks.Where(t => t.ServiceTypeID == 3)
+                                         .OrderByDescending(t => t.CreatedAt)
+                                         .ToList();
+
+                var uiItems = filteredTasks.Select(t =>
+                {
+                    bool isSuccess = t.OutputFileSize > 0;
+
+                    string fileSizeStr = t.OutputFileSize.HasValue
+                        ? $"{t.OutputFileSize.Value / 1024.0:F2} KB"
+                        : "0 KB";
+
+                    return new RemoveBackgroundTaskItem
+                    {
+                        FileName = Path.GetFileName(t.OriginalFilePath),
+                        Date = t.CreatedAt.ToString("g"),
+                        FileSize = fileSizeStr,
+                        Status = isSuccess ? "Completed" : "Failed",
+                        IsSuccess = isSuccess
+                    };
+                });
+
+                UserTasks = new ObservableCollection<RemoveBackgroundTaskItem>(uiItems);
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"Error fetching user tasks: {ex.Message}");
+            }
+            finally
+            {
+                IsBusy = false;
+            }
+        }
+
+        bool isZipFile(byte[] data)
+        {
+            if (data.Length < 2) return false;
+            return data[0] == 0x50 && data[1] == 0x4B;
+        }
+
+        private BitmapImage LoadBitmapImage(string path)
+        {
+            var bitmap = new BitmapImage();
+            bitmap.BeginInit();
+            bitmap.CacheOption = BitmapCacheOption.OnLoad;
+            bitmap.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
+            bitmap.EndInit();
+            bitmap.Freeze();
+            return bitmap;
         }
     }
-    
-    private async Task GetUserTasks()
+
+    public class RemoveBackgroundTaskItem
     {
-
-        try
-        {
-            IsBusy = true;
-            var tasks = await _taskService.GetUserTasksAsync();
-            var filteredTasks = tasks.Where(t => t.ServiceTypeID == 3).ToList();
-            UserTasks = new ObservableCollection<UserTasksDto>(filteredTasks);
-        }
-        catch (Exception ex)
-        {
-            Debug.WriteLine($"Error fetching user tasks: {ex.Message}");
-        }
-        finally
-        {
-            Debug.WriteLine($"User tasks fetched: {UserTasks.Count}");
-            IsBusy = false;
-        }
-    }
-
-    bool isZipFile(byte[] data)
-    {
-        if (data.Length < 2) return false;
-        return data[0] == 0x50 && data[1] == 0x4B;
-    }
-
-    private BitmapImage LoadBitmapImage(string path)
-    {
-        var bitmap = new BitmapImage();
-
-        // Bắt đầu quá trình khởi tạo
-        bitmap.BeginInit();
-
-        // OnLoad: Load toàn bộ ảnh vào bộ nhớ ngay lập tức và NHẢ file ra (không bị lock file)
-        bitmap.CacheOption = BitmapCacheOption.OnLoad;
-
-        bitmap.UriSource = new Uri(path, UriKind.RelativeOrAbsolute);
-
-        // Kết thúc khởi tạo
-        bitmap.EndInit();
-
-        // Freeze: Giúp Bitmap có thể truy cập từ các thread khác (nếu cần) và tối ưu hiệu năng
-        bitmap.Freeze();
-
-        return bitmap;
+        public string FileName { get; set; }
+        public string Date { get; set; }
+        public string FileSize { get; set; }
+        public string Status { get; set; }
+        public bool IsSuccess { get; set; }
     }
 }
